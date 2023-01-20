6283c8b49f68aeb5c60cff291c4092fed81120a0
James S <jamests1000@gmail.com>
2023-01-19 23:08:43 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # employee-trackerjs
+
+## James Simpson
+
+## Table of Contents
+- [Description](#description)
+- [Link to Video](#link-to-video)
+- [Link to Project](#link-to-project)
+
+## Description
+In this project I created an application would allow a user to create and delete notes, as well as store them on the side of the page. This app was deployed using heroku.
+
+## Link to Project
+[link](https://jsimps39.github.io/employee-trackerjs/)
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
 const mysql = require('mysql2');
+require('console.table');
 
 const db = mysql.createConnection({
     user: "root",
@@ -9,37 +10,45 @@ const db = mysql.createConnection({
 
 
 const chooseOption = (type) => {
-switch(type){
-    case 'View all employees': {
-     db.query('SELECT * FROM employee', (err, employees) => {
-    console.table(employees);
-});   
+    switch (type) {
+        case 'View all employees': {
+            db.query('SELECT * FROM employee', (err, employee) => {
+                console.table(employee);
+                init();
+            });
+            break;
+        }
+        case 'View all departments': {
+            db.query('SELECT * FROM department', (err, department) => {
+                console.table(department);
+                init();
+            });
+            break;
+        }
+        case 'View all roles': {
+            db.query('SELECT * FROM role', (err, role) => {
+                console.table(role);
+                init();
+            });
+            break;
+        }
     }
-    case 'View all departments':{
-        db.query('SELECT * FROM employee', (err, departments) => {
-    console.table(departments);
-});    
-}
-    case 'View all roles': {
-    db.query('SELECT * FROM employee', (err, roles) => {
-    console.table(roles);
-});
-}
-} 
 }
 
 const init = () => {
-prompt ({
-    type: 'rawList',
-    message: 'Chooose one of the following',
-    choices: [
-        'View all employees',
-        'View all departments',
-        'View all roles'
-    ],
-    name: 'type',
-})
-.then((answers) => {
-    chooseOption(answers.type);
-});
-}
+    prompt({
+        type: 'rawList',
+        message: 'Chooose one of the following',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+        ],
+        name: 'type',
+    })
+        .then((answers) => {
+            chooseOption(answers.type);
+        });
+};
+
+init();
